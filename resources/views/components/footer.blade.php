f6f6f8ba8ab1b97a276df71d790813b943d0aaab
Jake Bogan <theronbogan1@gmail.com>
2021-08-15 01:18:01 -0400

    adds footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="bg-blue-950">
+     <div class="max-w-screen-xl mx-auto text-white px-4 py-1">
+          <ul>
+               <li>
+                    <a href="#">Privacy Policy</a>
+               </li>
+               <li>
+                    <a href="#">Visit our Website</a>
+               </li>
+               <li>
+                    <a href="#">Copyright @ 2019-2021</a>
+               </li>
+          </ul>
+     </div>
+</footer>
